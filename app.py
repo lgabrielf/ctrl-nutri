0abc0657c15a111ea05c8f970c98883f29650051
Geowellex Hardware <projetos.hardware@geowellex.com>
2023-06-29 14:48:16 -0300

    add popup and monthly control

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
-import csv, barcode, os, zipfile
-from datetime import date
+import csv, barcode, os, zipfile, string, ctypes
 from barcode.writer import ImageWriter
+from datetime import datetime, timedelta
 
 PASTA_IMAGENS = os.path.abspath("barcode_item")
 
 st.set_page_config(page_title='MGJM - NutriControl', page_icon='logo_h.png')
+
 
 def login_page():
 
@@ -31,21 +32,43 @@ def login_page():
         else:
             st.error('Usuário ou senha incorretos.')
 
-def gerar_codigo_barras(nome_item):
+def popup_confirmacao():
+    result = ctypes.windll.user32.MessageBoxW(0, "Deseja registrar a entrada?", "Confirmação", 1)
+    return result == 1
 
-    codigo = barcode.get('code128', nome_item, writer=ImageWriter())
-    nome_arquivo = f"{nome_item}"
-    caminho_arquivo = os.path.join("barcode_item", nome_arquivo)
-    codigo.save(caminho_arquivo, options={'module_width': 0.4, 'module_height': 8.0})
+def verificar_item_existente(nome, unidade, estoque):
+    for item in estoque:
+        if len(item) >= 3 and item[0].lower() == nome.lower() and item[2].lower() == unidade.lower():
+            return True
+    return False
 
-    return nome_arquivo
+def gerar_codigo_barras(nome, unidade):
+    nome_limpo = nome.translate(str.maketrans('', '', string.punctuation)).replace(' ', '').lower()
+    unidade_limpa = unidade.translate(str.maketrans('', '', string.punctuation)).replace(' ', '').lower()
+    codigo = barcode.get('code128', nome_limpo + '_' + unidade_limpa, writer=ImageWriter())
+    barcode_folder_path = os.path.join(os.getcwd(), PASTA_IMAGENS)
+    os.makedirs(barcode_folder_path, exist_ok=True)
+    barcode_path = os.path.join(barcode_folder_path, nome_limpo + '_' + unidade_limpa)
+    codigo.save(barcode_path)
+    return nome, nome_limpo
+
 
 def calcular_consumo_mensal(estoque):
     consumo_mensal = {}
+    hoje = datetime.now()
+
     for item in estoque[1:]:
         codigo_barras = item[3]
-        quantidade = int(item[1]) if item[1] else 0
-        consumo_mensal[codigo_barras] = consumo_mensal.get(codigo_barras, 0) + quantidade
+        saidas = int(item[5]) if item[5] else 0
+        data_ultima_saida = item[6]
+
+        if saidas > 0 and data_ultima_saida:
+            data_ultima_saida = datetime.strptime(data_ultima_saida, "%Y-%m-%d %H:%M:%S")
+            dias_passados = (hoje - data_ultima_saida).days
+
+            if dias_passados <= 30:
+                consumo_mensal[codigo_barras] = consumo_mensal.get(codigo_barras, 0) + saidas
+
     return consumo_mensal
 
 def ler_estoque():
@@ -133,7 +156,7 @@ def main():
                 item.extend([consumo, dias_restantes])
 
             # Criar uma cópia do estoque com as colunas adicionais
-            estoque_display = [["Nome do Produto", "Quantidade", "Unidade", "Código de Barras", "Estoque de Segurança", "Saída", "Consumo Mensal", "Dias Restantes"]] + estoque[1:]
+            estoque_display = [["Nome do Produto", "Quantidade", "Unidade", "Código de Barras", "Estoque de Segurança", "Saída", "Consumo Mensal", "Dias Restantes"]] + [item[:6] + item[7:] for item in estoque[1:]]
             
             # Exibir a tabela do estoque
             st.table(estoque_display)
@@ -141,7 +164,7 @@ def main():
         elif menu_selecionado == 'Cadastrar Novo Item' and st.session_state['permissions'] == 'admin':
             st.header('Cadastrar Novo Item ao Estoque')
             nome_produto = st.text_input('Nome do Produto')
-            unidade = st.selectbox('Unidade do Item',('KG', 'DUZIA (PACOTE)', 'CAIXA', 'UNIDADE'))
+            unidade = st.selectbox('Unidade do Item', ('KG', 'DUZIA (PACOTE)', 'CAIXA', 'UNIDADE'))
             estoque_seguranca = st.number_input('Estoque de segurança do Item', min_value=0)
 
             if st.button('Cadastrar'):
@@ -151,10 +174,12 @@ def main():
                     st.warning('O campo "Unidade do Produto" é obrigatório.')
                 elif estoque_seguranca is None or estoque_seguranca < 0:
                     st.warning('O campo "Estoque de Segurança" deve ser preenchido com um valor válido.')
+                elif verificar_item_existente(nome_produto, unidade, estoque):
+                    st.warning('O item já existe no estoque.')
                 else:
-                    codigo_produto = gerar_codigo_barras(nome_produto)
+                    nome_produto, nome_produto_limpo = gerar_codigo_barras(nome_produto, unidade)
 
-                    novo_item = [nome_produto, '0', unidade, codigo_produto, estoque_seguranca, '', '']
+                    novo_item = [nome_produto, '0', unidade, nome_produto_limpo, estoque_seguranca, '0', '']
                     estoque.append(novo_item)
                     gravar_estoque(estoque)
                     st.success('Item cadastrado com sucesso.')
@@ -186,8 +211,8 @@ def main():
 
             quantidade = st.number_input('Quantidade', min_value=1, value=1)
             col1, col2 = st.columns(2)
-            adicionar_button = col1.button('Adicionar')
-            remover_button = col2.button('Remover')
+            adicionar_button = col1.button('Adicionar', key='adicionar')
+            remover_button = col2.button('Remover', key='remover')
 
             if adicionar_button:
                 item = encontrar_produto_por_codigo(codigo, estoque)
@@ -206,6 +231,7 @@ def main():
                         st.warning('A quantidade a remover é maior do que a disponível no estoque.')
                     else:
                         item[1] = str(new_quantity)
+                        item[5] = str(int(item[5]) + 1)
                         gravar_estoque(estoque)
                         st.success(f'Foram removidas {quantidade} unidades do estoque do item {item[0]}.')
                 else:
@@ -215,18 +241,20 @@ def main():
             st.write('<style>div.entry-header { background-color: blue; padding: 10px; border-radius: 5px; color: white; font-size: 20px; font-weight: bold; }</style>', unsafe_allow_html=True)
             st.write('<div class="entry-header">Entrada de Produtos</div>', unsafe_allow_html=True)
             "---"
-            
 
-            codigo = st.text_input('Código de Barras do Item')
-            item = encontrar_produto_por_codigo(codigo, estoque)
+            codigo1 = st.text_input('Código de Barras do Item')
+            item = encontrar_produto_por_codigo(codigo1, estoque)
 
             if item is not None:
                 quantidade = 1
-
                 if st.button('Registrar entrada'):
-                    item[1] = str(int(item[1]) + 1)
-                    gravar_estoque(estoque)
-                    st.success("Entrada registrada com sucesso.")
+                    confirmado = popup_confirmacao()
+                    if confirmado:
+                        item[1] = str(int(item[1]) + 1)
+                        gravar_estoque(estoque)
+                        st.success("Entrada registrada com sucesso.")
+                    else:
+                        st.info("A entrada de produtos não foi registrada.")
             else:
                 st.warning("Item não encontrado.")
 
@@ -245,10 +273,14 @@ def main():
                 if st.button('Registrar saída'):
                     quantidade = int(item[1])
                     if quantidade > 0:
-                        item[1] = str(quantidade-1)
+                        item[1] = str(quantidade - 1)
                         item[5] = str(int(item[5]) + 1)
+                        item[6] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Atualiza a data da última saída
                         gravar_estoque(estoque)
                         st.success("Saída registrada com sucesso.")
+
+                        # Atualiza o consumo mensal
+                        consumo_mensal = calcular_consumo_mensal(estoque)
                     else:
                         st.warning("Não há estoque disponível desse item.")
             else:
